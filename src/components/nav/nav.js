71ebed74c956e94164991f8d714f8e27d37dc325
Clifford Nelson <cliffordryannelson@gmail.com>
2023-07-20 17:29:19 -0700

    better header nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,117 +3,102 @@ import { NavLink } from 'react-router-dom';
 import { gsap } from 'gsap';
 import './nav.scss';
 
-function Nav() {
-  useEffect(() => {
-    let isOpen = window.innerWidth <= 768 ? false : true;
-    let nav = document.querySelector('#nav');
-    let hamburger = document.querySelector('#hamburger');
-    let hamburgerWrapper = document.querySelector('#wrapper');
-    let hamburgerBarsList = document.getElementsByClassName('bars');
-    let hamburgerBars = Array.from(hamburgerBarsList);
-    let navItemsWrapper = document.querySelector('#navItems');
-    let navItemsList = document.getElementsByClassName('nav-item');
-    let navItemElements = Array.from(navItemsList);
+const Nav = () => {
 
-    let toggleNav = () => {
-      if (isOpen) {
+  useEffect(() => {
+    const MOBILE_BREAKPOINT = 768;
+    let isOpen = window.innerWidth <= MOBILE_BREAKPOINT;
+    const nav = document.querySelector('#nav');
+    const hamburger = document.querySelector('#hamburger');
+    const hamburgerWrapper = document.querySelector('#wrapper');
+    const hamburgerBars = Array.from(document.getElementsByClassName('bars'));
+    const navItemsWrapper = document.querySelector('#navItems');
+    const navItemElements = Array.from(document.getElementsByClassName('nav-item'));
+    
+    const clickToClose = () => {
+      if (window.innerWidth <= MOBILE_BREAKPOINT) { 
         closeNav();
-      } else {
-        openNav();
       }
     };
 
-    let openNav = () => {
+    navItemElements.forEach((navItem) => {
+      navItem.addEventListener('click', clickToClose);
+    });
+
+    const freezeScroll = () => { 
+      if (window.innerWidth <= MOBILE_BREAKPOINT) { 
+        document.body.style.overflow = 'hidden' 
+      }
+    }
+    const releaseScroll = () => { 
+      document.body.style.overflow = 'auto'
+    }
+
+    const toggleNav = () => isOpen ? closeNav() : openNav();
+
+    const openNav = () => {
       animateTheNav('open');
+      freezeScroll();
       nav.classList.add('active');
       isOpen = true;
     };
 
-    let closeNav = () => {
+    const closeNav = () => {
       animateTheNav('close');
+      releaseScroll();
       nav.classList.remove('active');
       isOpen = false;
     };
 
-    let animateTheNav = (direction) => {
-      if (direction === 'open') {
-        animationTimeline.play();
-      } else {
-        animationTimeline.reverse();
-      }
+    const animateTheNav = (direction) => {
+      direction === 'open' ? animationTimeline.play() : animationTimeline.reverse();
     };
 
-    let animationTimeline = gsap
-      .timeline({
-        paused: true,
-        defaults: {
-          duration: 0.5,
-          ease: 'power1.out',
-        },
-      })
+    const animationTimeline = gsap.timeline({
+      paused: true,
+      defaults: { duration: 0.5, ease: 'power1.out' }
+    })
       .to(nav, {}, '<')
-      .to(navItemsWrapper, {
-        maxWidth: '100vw',
-      }, '<')
-      .to(navItemElements, {
-        stagger: 0.05,
-        x: 0,
-        autoAlpha: 1,
-      }, '<')
-      .to(hamburgerWrapper, {
-        rotateZ: -90,
-      }, '<0.1')
+      .to(navItemsWrapper, { maxWidth: '100vw' }, '<')
+      .to(navItemElements, { stagger: 0.05, x: 0, autoAlpha: 1 }, '<')
+      .to(hamburgerWrapper, { rotateZ: -90 }, '<0.1')
       .to(hamburgerBars, {}, '<')
-      .to([hamburgerBars[0], hamburgerBars[3]], {
-        rotateZ: -90,
-        scale: 0,
-        autoAlpha: 0,
-      }, '<')
-      .to(hamburgerBars[0], {
-        y: '1rem',
-      }, '<')
-      .to(hamburgerBars[3], {
-        y: '-1rem',
-      }, '<')
-      .to(hamburgerBars[1], {
-        rotate: -45,
-      }, '<')
-      .to(hamburgerBars[2], {
-        rotate: -135,
-      }, '<');
+      .to([hamburgerBars[0], hamburgerBars[3]], { rotateZ: -90, scale: 0, autoAlpha: 0 }, '<')
+      .to(hamburgerBars[0], { y: '1rem' }, '<')
+      .to(hamburgerBars[3], { y: '-1rem' }, '<')
+      .to(hamburgerBars[1], { rotate: -45 }, '<')
+      .to(hamburgerBars[2], { rotate: -135 }, '<');
 
-    gsap.set(navItemElements, {
-      x: '4ch',
-      autoAlpha: 0,
-    });
-    gsap.set(navItemsWrapper, {
-      maxWidth: 0,
-    });
+    gsap.set(navItemElements, { x: '4ch', autoAlpha: 0 });
+    gsap.set(navItemsWrapper, { maxWidth: 0 });
 
     hamburger.addEventListener('click', toggleNav);
 
-    window.addEventListener('resize', function () {
-      if (window.innerWidth <= 576) {
+    const handleResize = () => {
+      if (window.innerWidth <= MOBILE_BREAKPOINT) {
         closeNav();
       } else {
         openNav();
       }
-    });
+    };
 
-    window.addEventListener('load', function () {
-      this.setTimeout(() => {
-        if (window.innerWidth <= 576) {
+    const handleLoad = () => {
+      setTimeout(() => {
+        if (window.innerWidth <= MOBILE_BREAKPOINT) {
           closeNav();
         } else {
           openNav();
         }
       }, 800);
-    });
+    };
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('load', handleLoad);
 
     return () => {
       hamburger.removeEventListener('click', toggleNav);
-      window.removeEventListener('resize', () => {});
-      window.removeEventListener('load', () => {});
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('load', handleLoad);
       animationTimeline.kill();
     };
   }, []);
@@ -123,24 +108,23 @@ function Nav() {
   return (
     <nav id="nav" className="nav">
       <div id="navItems" className="nav-items">
-          <NavLink activeClassName="nav-item--active" className="nav-item" exact to="/">Home</NavLink>
-          <NavLink activeClassName="nav-item--active" className="nav-item" to="/about">About</NavLink>
-          <NavLink activeClassName="nav-item--active" className="nav-item" to="/resume">Resume</NavLink>
-          <NavLink activeClassName={currentPath.includes('/projects/') ? '' : "nav-item--active"} className="nav-item" to="/projects">Projects</NavLink>
+        <NavLink activeClassName="nav-item--active" className="nav-item" exact to="/">Home</NavLink>
+        <NavLink activeClassName="nav-item--active" className="nav-item" to="/about">About</NavLink>
+        <NavLink activeClassName="nav-item--active" className="nav-item" to="/resume">Resume</NavLink>
+        <NavLink activeClassName={currentPath.includes('/projects/') ? '' : "nav-item--active"} className="nav-item" to="/projects">Projects</NavLink>
+      </div>
+
+      <button id="hamburger" className="hamburger">
+        <div id="wrapper" className="wrapper">
+          <span className="bars bar-1"></span>
+          <span className="bars bar-2a"></span>
+          <span className="bars bar-2b"></span>
+          <span className="bars bar-3"></span>
         </div>
+      </button>
 
-        <button id="hamburger" className="hamburger">
-          <div id="wrapper" className="wrapper">
-            <span className="bars bar-1"></span>
-            <span className="bars bar-2a"></span>
-            <span className="bars bar-2b"></span>
-            <span className="bars bar-3"></span>
-          </div>
-        </button>
-
-      </nav>
-    );
-  
+    </nav>
+  );
 }
 
 export default Nav;
