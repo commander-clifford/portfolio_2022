0aa132c9cdbfd740f4254533404702800a5fe5a6
Clifford Nelson <cliffordryannelson@gmail.com>
2023-09-12 10:49:54 -0700

    remove unused code from file separation

--- src/services/contentfulAPI.js
+++ src/services/contentfulAPI.js
@@ -1,14 +1,5 @@
 import { createClient } from 'contentful';
-import Band from '../components/band/band';
-import Cover from '../components/cover/cover';
-import Button from '../components/button/button';
-import Spotlight from '../components/spotlight/spotlight';
-import Card from '../components/card/card';
-import Poster from '../components/poster/poster';
-import Text from '../components/text/text';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { CONFIG } from './config.js';
-
 
 const client = createClient({
   space: CONFIG.space, // This is the space ID. A space is like a project folder in Contentful terms
@@ -29,5 +20,3 @@ export function fetchContentfulAsset(id) {
   const asset = client.getAsset(id);
   return asset;
 }
-
-
