ab56de787c75f8eef314b899dbb28df09c0970ad
Clifford Nelson <cliffordryannelson@gmail.com>
2023-10-10 19:21:56 -0700

    update components

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from '../button/button';
 import Text from '../text/text';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import './card.scss';
 
 const Card = ({ data = {} }) => {
-  const { alignment = '', height = '', type = '', subHeadline = '', headline = '', additionalClassNames = '' } = data;
+  // Destructure properties from the incoming data prop with default values
+  const { 
+    alignment = '', 
+    height = '', 
+    type = '', 
+    subHeadline = '', 
+    headline = '', 
+    additionalClassNames = '' 
+  } = data;
 
-  console.log('CARD alignment',alignment);
-
+  // Mapping for height types to their respective CSS classes
   const heightMapping = {
     'Flat': 'shadow-none',
     'Level 1': 'shadow-sm',
@@ -17,39 +23,52 @@ const Card = ({ data = {} }) => {
     'Level 3': 'shadow-lg'
   };
 
+  // Mapping for text alignment to their respective CSS classes
   const alignmentClassMap = {
     "Left": "text-left",
     "Right": "text-right",
     "Center": "text-center"
   };
-  
-  const alignmentClass = alignmentClassMap[alignment] || "text-left";
 
+  // Get the appropriate alignment class
+  const alignmentClass = alignmentClassMap[alignment] || "";
+
+  // Get the appropriate height class
   const formattedHeight = heightMapping[height] || '';
 
+  // Function to render the card's content based on the type of item
   const renderCardContent = (item, i) => {
+
+    // Get the type of the content
     const componentType = item?.sys?.contentType?.sys?.id;
 
+    // Check if the component type is 'text'
     if (componentType === 'text') {
       return (
+        // Render the Text component
         <Text key={i} parentAlignment={alignment} data={item?.fields}>
           {documentToReactComponents(item.fields.copy)}
         </Text>
       );
+    // Check if the component type is 'button'
     } else if (componentType === 'button') {
       return (
+        // Render the Button component
         <Button classNames={alignment} key={i} data={item?.fields} alignment={alignment} />
       );
     } else {
+      // Log an error for unsupported component types
       console.error(`Error: unsupported componentType ${componentType}`);
       return null;
     }
   };
 
+  // Build the class string
   const classes = ['card', type, formattedHeight, additionalClassNames, alignmentClass].filter(Boolean).join(' ');
 
   return (
-    <div className={classes}>
+    // Render the Card component
+    <div className={classes} tabIndex={'0'}>
       <div className={'card__headline'}>
         {subHeadline && <h5>{subHeadline}</h5>}
         {headline && <h2>{headline}</h2>}
--- src/components/poster/poster.js
+++ src/components/poster/poster.js
@@ -1,44 +1,37 @@
 import React from 'react';
-import Button from '../button/button';
-import Text from '../text/text';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import './poster.scss';
 
 const Poster = ({ data = {} }) => {
 
-  // console.log("Poster DATA:", data);
-
-  const { zHeight = '', type = '', subHeadline = '', headline = '', classNames = '', cardContent, cta } = data;
+  const {
+    zHeight = '',
+    type = '',
+    subHeadline = '',
+    headline = '',
+    classNames = '',
+    posterContent,
+  } = data;
 
   const heightMapping = {
-    'Level 0': 'shadow-none',
     'Level 1': 'shadow-sm',
     'Level 2': 'shadow',
     'Level 3': 'shadow-lg'
   };
 
-  const formattedHeight = heightMapping[zHeight] || 'zero';
+  const formattedHeight = heightMapping[zHeight] || '';
 
-  const classes = ['poster card', type, formattedHeight, classNames].filter(Boolean).join(' ');
+  const classes = ['poster', type, formattedHeight, classNames].filter(Boolean).join(' ');
 
   return (
-    <div className={classes}>
-      <div className={'poster__headline'}>
+    <div className={classes} tabIndex={0}>
+      <div className="poster__headline">
         {subHeadline && <h5>{subHeadline}</h5>}
         {headline && <h2>{headline}</h2>}
       </div>
-      <div className={'poster__copy'}>
-        <Text>
-          {documentToReactComponents(cardContent)}
-        </Text>
+      <div className="poster__copy">
+        {posterContent && documentToReactComponents(posterContent)}
       </div>
-      {cta ? 
-        <Button className={'poster__cta'}>
-          CTA Button
-        </Button> 
-        : null
-      }
-      
     </div>
   );
 };
